fbc5e5cf65f1235cddf3fda6b9987fc12380fc87
Kevat <kevatshah@gmail.com>
2016-03-28 09:46:47 -0400

    Fixed
    Fixed

--- Assignment2_686/problems/2objects.js
+++ Assignment2_686/problems/2objects.js
@@ -7,6 +7,25 @@ var program;
 
 var NumVertices = 36;
 
+var near = -1;
+var far = 1;
+var radius = 1.0;
+var theta  = 0.0;
+var phi    = 0.0;
+var dr = 5.0 * Math.PI/180.0;
+
+var left = -1.0;
+var right = 1.0;
+var ytop = 1.0;
+var bottom = -1.0;
+
+
+var mvMatrix, pMatrix;
+var modelView, projection;
+var eye;
+
+const at = vec3(0.0, 0.0, 0.0);
+const up = vec3(0.0, 1.0, 0.0);
 
 window.onload = function init(){
 //    try {
@@ -33,7 +52,7 @@ window.onload = function init(){
         console.log(myCube.TriangleVertices);  // debugging line
         var cBuffer = gl.createBuffer();
         gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-        gl.bufferData( gl.ARRAY_BUFFER, flatten(myCube.VertexColors), gl.STATIC_DRAW );
+        gl.bufferData( gl.ARRAY_BUFFER, flatten(myCube.TriangleVertexColors), gl.STATIC_DRAW );
 
         var vColor = gl.getAttribLocation( program, "vColor" );
         gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
@@ -48,8 +67,8 @@ window.onload = function init(){
         gl.enableVertexAttribArray( vPosition );
 
         // Draw it
-        gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
-        //render()
+        //gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
+        render();
 //    } catch (e) {
 //      alert("Could not initialize WebGL! " + e);
 //      return;
@@ -58,6 +77,16 @@ window.onload = function init(){
 
 function render(){
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+            
+        eye = vec3(radius*Math.sin(theta)*Math.cos(phi), 
+        radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta));
 
-    gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
+        mvMatrix = lookAt(eye, at , up); 
+        pMatrix = ortho(left, right, bottom, ytop, near, far);
+        
+        gl.uniformMatrix4fv( modelView, false, flatten(mvMatrix) );
+        gl.uniformMatrix4fv( projection, false, flatten(pMatrix) );
+            
+        gl.drawArrays( gl.TRIANGLES, 0, myCube.TriangleVertices.length );
+        requestAnimFrame(render);
 }
